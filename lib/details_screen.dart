a299d9b6977193b5615f0b48245a3791cc741c3d
Liliana Faustino <lifaustino@hotmail.com>
2022-01-14 15:54:21 +0000

    details

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:weather/models/daily.dart';
 
 class DetailsScreen extends StatefulWidget{
   const DetailsScreen({Key? key}) : super(key: key);
@@ -9,22 +11,177 @@ class DetailsScreen extends StatefulWidget{
 }
 
 class _DetailsScreenState extends State<DetailsScreen>{
+  late final Daily _weather = ModalRoute.of(context)!.settings.arguments as Daily;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Vista de detalhes'),
-      ),
-      body: Center(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/${_weather.info.main.toLowerCase()}.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('Isto vai ser a vista de detalhes')
+          children: [
+            weatherBox(),
+            detailsBox()
           ],
         ),
       ),
     );
   }
 
+  Widget weatherBox() {
+    return FutureBuilder(
+        builder: (context, snapshot){
+          return Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Column(
+                children: <Widget> [
+                  Text(DateFormat.EEEE().format(new DateTime.fromMillisecondsSinceEpoch(_weather.dt * 1000)),
+                      style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold)
+                  ),
+                  getWeatherIcon(_weather.info.icon),
+                  Text(
+                      'Min: ${_weather.min.round()} ºC Max: ${_weather.max.round()} ºC',
+                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)
+                  ),
+                ]
+            ),
+          );
+        });
+  }
 
+  Widget detailsBox() {
+    return FutureBuilder(
+        builder: (context, snapshot){
+          return Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Container(
+              width: 320.0,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget> [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: [
+                            getSmallWeatherIcon('sunrise'),
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Expanded(child:
+                                Text('Sunrise',
+                                    textAlign: TextAlign.left,
+                                  style: TextStyle(color: Colors.white, fontSize: 20)),
+                              ),
+                            ),
+                            Expanded(child :
+                            Text(
+                                '${DateFormat.Hm().format(new DateTime.fromMillisecondsSinceEpoch(_weather.sunrise * 1000))}',
+                                textAlign: TextAlign.right,
+                                style: TextStyle(color: Colors.white, fontSize: 20)
+                            ),
+                            ),
+                          ]),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                            children: [
+                              getSmallWeatherIcon('sunset'),
+                              Expanded(child:
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text('Sunset',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(color: Colors.white, fontSize: 20)),
+                              ),
+                              ),
+                              Expanded(child :
+                              Text(
+                                  '${DateFormat.Hm().format(new DateTime.fromMillisecondsSinceEpoch(_weather.sunset * 1000))}',
+                                  textAlign: TextAlign.right,
+                                  style: TextStyle(color: Colors.white, fontSize: 20)
+                              ),
+                              ),
+                            ]),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                            children: [
+                              getSmallWeatherIcon('humidity'),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Expanded(child:
+                                Text('Humidity',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(color: Colors.white, fontSize: 20)),
+                                ),
+                              ),
+                              Expanded(child :
+                              Text(
+                                  '${_weather.humidity}%',
+                                  textAlign: TextAlign.right,
+                                  style: TextStyle(color: Colors.white, fontSize: 20)
+                              ),
+                              ),
+                            ]),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                            children: [
+                              getSmallWeatherIcon('wind'),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Expanded(child:
+                                Text('Wind',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(color: Colors.white, fontSize: 20)),
+                                ),
+                              ),
+                              Expanded(child :
+                              Text(
+                                  '${_weather.wind}Km/h',
+                                  textAlign: TextAlign.right,
+                                  style: TextStyle(color: Colors.white, fontSize: 20)
+                              ),
+                              ),
+                            ]),
+                      ),
+                    ]
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
+  Image getWeatherIcon(String _icon) {
+    String path = 'assets/icons/';
+    String imageExtension = ".png";
+    return Image.asset(
+      path + _icon + imageExtension,
+      width: 200,
+      height: 200,
+    );
+  }
+
+  Image getSmallWeatherIcon(String _icon) {
+    String path = 'assets/icons/';
+    String imageExtension = ".png";
+    return Image.asset(
+      path + _icon + imageExtension,
+      width: 30,
+      height: 30,
+    );
+  }
 }
--- lib/models/daily.dart
+++ lib/models/daily.dart
@@ -6,10 +6,18 @@ class Daily {
         dt = json['dt'],
         min = json['temp']['min'].toDouble(),
         max = json['temp']['max'].toDouble(),
+        sunrise = json['sunrise'],
+        sunset = json['sunset'],
+        humidity = json['humidity'].toDouble(),
+        wind = json['wind_speed'].toDouble(),
         info = Info.fromJson(json['weather'][0]);
 
   final int dt;
   final double min;
   final double max;
+  final int sunrise;
+  final int sunset;
+  final double humidity;
+  final double wind;
   final Info info;
 }
